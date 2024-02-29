37172f7f5f74f6b6267e030a5134782698f6f8a7
amabily <amabilimangolin123@gmail.com>
2024-02-29 12:03:23 -0300

    Meu Primeiro Commit

---
+++ bagulho/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace bagulho;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
